8efdfa2ad09eb71867a3007e31772251245d17d2
BadRyuner <an.kujel@yandex.ru>
2022-08-10 14:12:53 +0300

    Update PlayerShip.razor

--- PLCSE/Helpers.cs
+++ PLCSE/Helpers.cs
@@ -13,11 +13,13 @@
 		private static string[] MainTurretNames = new[] { "Main Turret", "CU Long Range", "Keeper Beam", "RapidFire", "W.D. Prototype \"FlashFire\"", "Modified CU Long Range", "Retrofitted Coil Gun", "URV Heavy Turret" };
 		private static string[] ProgramNames = new[] { "Sitting Duck [VIRUS]", "Warp Disable [VIRUS]", "Phalanx [VIRUS]", "Backdoor [VIRUS]", "Emergency Shield Boosting", "Instant Warp Charge", "Instant System Self-Healing", "Instant AntiVirus", "Gentleman's Welcome [VIRUS]", "Siphon Credits [VIRUS]", "0x84B7A2 [VIRUS]", "0xF592B1 [VIRUS]", "Capacitor", "Thrust Booster", "Detector", "Block Long Range Comms", "Barrage", "OverDrive", "Syber's Shield [VIRUS]", "Syber's Threat", "Armor Flaw [VIRUS]", "Shutdown Defenses [VIRUS]", "Discharge", "Digital Coolant", "Quantum Defenses", "Shock The System", "Reinstall++", "Quantum Tunnel", "Extended Shields", "Burst AV", "Sylvassi Capacitor", "Random Access", "Active Reset", "SyberWar", "Military-Grade Shield Boosting", "EMP", "Depressurize", "Fix", "Drain", };
 		private static string[] NukeNames = new[] { "W.D. Small", "W.D. Large", "C.U. Enforcer", "C.U. Peacekeeper", "Project Megaton", "Tactical Nuke" };
-		private static string[] TrackerNames = new[] { "Tracker Missile", "W.D. Special", "Shield Piercer", "Reactor Buster", "Straight Shot", "Acid Missile", "Armor Piercing Missile", "System Damage Missile", "F.B. Missile", "Torpedo" };
+		private static string[] TrackerNames = new[] { "Tracker Missile", "W.D. Special", "Shield Piercer", "MAX", "SHIELD_FOCUS", "Reactor Buster", "Straight Shot", "Acid Missile", "Armor Piercing Missile", "System Damage Missile", "F.B. Missile", "Torpedo" };
 		private static string[] InertiaThrusterNames = new[] { "Inertia Thruster", "Mini Inertia Thruster", "Infected Inertia Thruster", "Super Inertia Thruster", "Gimbal Inertia Thruster" };
 		private static string[] ManeuverThrusterNames = new[] { "Maneuvering Thruster", "Heavy Maneuvering Thruster" , "Racing Maneuvering Thruster" };
 		private static string[] CaptainChairNames = new[] { "Colonial Classic Captain's Chair", "Colonial Modern Captain's Chair", "W.D. Classic Captain's Chair" };
 		private static string[] ExtractorNames = new[] { "StarSalvage E40", "StarSalvage E70", "The Remover", "StarSalvage E900", "P.T. Extractor"};
+		private static string[] PolytechModuleNames = new[] { "P.T. Module: Shields", "P.T. Module: Damage", "P.T. Module: Quantum Charger", "P.T. Module: Rapid Assembly", "P.T. Module: Crew Repair Nano-bots", "P.T. Module: Thrust" };
+		private static string[] FBRecipeNames = new[] { "Classic Biscuit Recipe", "Emergency Biscuit Recipe", "Healthy Biscuit Recipe", "Carrot Cake Biscuit Recipe", "Hearty Biscuit Recipe", "Whole Wheat Biscuit Recipe", "Healberry Crumbly Biscuit Recipe", "Sugar Biscuit Recipe", "Garlic Biscuit Recipe", "Frozen Biscuit Recipe", "Spicy Biscuit Recipe", "Spicy Crumbly Biscuit Recipe", "Grenade Damage Biscuit Recipe", "Lucky Biscuit Recipe", "Molten Biscuit Recipe", "Long Life Biscuit Recipe", "Funky Biscuit Recipe" };
 
 		public static string DecodeNameForComponent(uint inHash)
 		{
@@ -44,13 +46,15 @@
 					case 7:
 						return CPUNames[subtype] + $" {level} lvl";
 					case 8:
-						return $"{(type == 0 ? "Standard O2 Generator" : type == 1 ? "\"Calypso\" O2 Generator" : "Modded Oxygen Generator")} {level} lvl";
+						return $"{(subtype == 0 ? "Standard O2 Generator" : subtype == 1 ? "\"Calypso\" O2 Generator" : "Modded Oxygen Generator")} {level} lvl";
 					case 9:
 						return ThrusterNames[subtype] + $" {level} lvl";
 					case 10:
 						return TurretNames[subtype] + $" {level} lvl";
 					case 11:
 						return MainTurretNames[subtype] + $" {level} lvl";
+					case 16:
+						return $"HullPlating {level} lvl";
 					case 17:
 						return ProgramNames[subtype] + $" {level} lvl";
 					case 19:
@@ -59,6 +63,8 @@
 						return TrackerNames[subtype] + $" {level} lvl";
 					case 21:
 						return $"Scrap {level} lvl";
+					case 22:
+						return $"{(subtype == 0 ? "400 MHz" : subtype == 1 ? "580.8 MHz" : "200 MHz")} {level} lvl";
 					case 23:
 						return "Unknown Mission Component";
 					case 24:
@@ -71,12 +77,18 @@
 						return CaptainChairNames[subtype] + $" {level} lvl";
 					case 28:
 						return ExtractorNames[subtype] + $" {level} lvl";
+					case 30:
+						return FBRecipeNames[subtype] + $" {level} lvl";
 					case 31:
 						return $"Biscuit Bomb {level} lvl";
 					case 32:
 						return $"Sensord Dish {level} lvl";
 					case 33:
 						return $"{(subtype == 1 ? "Sylvassi" : null)} Cloaking System {level} lvl";
+					case 34:
+						return PolytechModuleNames[subtype] + $" {level} lvl";
+					case 35:
+						return $"Powered Armor {level} lvl";
 					default:
 						return $"Unknown Component {level} lvl";
 				}
